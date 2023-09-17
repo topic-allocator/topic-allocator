7077b539759d814bd663b61e69e6f83e3067c5d9
landris006 <laczko.andris007@gmail.com>
2023-09-17 15:37:31 +0200

    version bump + deploy script changes

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,13 @@ echo "Client succesfully built and moved to 'server/static'"
 
 rm -rf .temp
 mkdir .temp
+
 cp -r server .temp
+rm -rf .temp/server/dist .temp/server/node_modules
+
 npm install --prefix .temp/server
 npm run generate --prefix .temp/server
+npm run build --prefix .temp/server
+
 npm run publish --prefix .temp/server
 rm -rf .temp
-
-echo "Server succesfully built and published to azure"
