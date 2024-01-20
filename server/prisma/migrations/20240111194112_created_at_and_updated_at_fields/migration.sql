58cfdc9e0fa12d81b3237405e37de1066dfaeede
landris006 <laczko.andris007@gmail.com>
2024-01-20 10:25:07 +0100

    migration fix

--- server/prisma/migrations/20240111194112_created_at_and_updated_at_fields/migration.sql
+++ server/prisma/migrations/20240111194112_created_at_and_updated_at_fields/migration.sql
@@ -16,32 +16,32 @@ BEGIN TRAN;
 
 -- AlterTable
 ALTER TABLE [dbo].[course] ADD [created_at] DATETIME2 NOT NULL CONSTRAINT [course_created_at_df] DEFAULT CURRENT_TIMESTAMP,
-[updated_at] DATETIME2 NOT NULL;
+[updated_at] DATETIME2 NOT NULL CONSTRAINT [course_updated_at_df] DEFAULT CURRENT_TIMESTAMP;
 
 -- AlterTable
 ALTER TABLE [dbo].[instructor] ADD [created_at] DATETIME2 NOT NULL CONSTRAINT [instructor_created_at_df] DEFAULT CURRENT_TIMESTAMP,
-[updated_at] DATETIME2 NOT NULL;
+[updated_at] DATETIME2 NOT NULL CONSTRAINT [instructor_updated_at_df] DEFAULT CURRENT_TIMESTAMP;
 
 -- AlterTable
 ALTER TABLE [dbo].[student] ADD [created_at] DATETIME2 NOT NULL CONSTRAINT [student_created_at_df] DEFAULT CURRENT_TIMESTAMP,
-[updated_at] DATETIME2 NOT NULL;
+[updated_at] DATETIME2 NOT NULL CONSTRAINT [student_updated_at_df] DEFAULT CURRENT_TIMESTAMP;
 
 -- AlterTable
 ALTER TABLE [dbo].[student_course_completion] ALTER COLUMN [grade] FLOAT(53) NOT NULL;
 ALTER TABLE [dbo].[student_course_completion] ADD [created_at] DATETIME2 NOT NULL CONSTRAINT [student_course_completion_created_at_df] DEFAULT CURRENT_TIMESTAMP,
-[updated_at] DATETIME2 NOT NULL;
+[updated_at] DATETIME2 NOT NULL CONSTRAINT [student_course_completion_updated_at_df] DEFAULT CURRENT_TIMESTAMP;
 
 -- AlterTable
 ALTER TABLE [dbo].[student_topic_preference] ADD [created_at] DATETIME2 NOT NULL CONSTRAINT [student_topic_preference_created_at_df] DEFAULT CURRENT_TIMESTAMP,
-[updated_at] DATETIME2 NOT NULL;
+[updated_at] DATETIME2 NOT NULL CONSTRAINT [student_topic_preference_updated_at_df] DEFAULT CURRENT_TIMESTAMP;
 
 -- AlterTable
 ALTER TABLE [dbo].[topic] ADD [created_at] DATETIME2 NOT NULL CONSTRAINT [topic_created_at_df] DEFAULT CURRENT_TIMESTAMP,
-[updated_at] DATETIME2 NOT NULL;
+[updated_at] DATETIME2 NOT NULL CONSTRAINT [topic_updated_at_df] DEFAULT CURRENT_TIMESTAMP;
 
 -- AlterTable
 ALTER TABLE [dbo].[topic_course_preference] ADD [created_at] DATETIME2 NOT NULL CONSTRAINT [topic_course_preference_created_at_df] DEFAULT CURRENT_TIMESTAMP,
-[updated_at] DATETIME2 NOT NULL;
+[updated_at] DATETIME2 NOT NULL CONSTRAINT [topic_course_preference_updated_at_df] DEFAULT CURRENT_TIMESTAMP;
 
 COMMIT TRAN;
 
