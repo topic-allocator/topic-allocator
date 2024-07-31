3914bba07a7569e4e2516ebf00042578fff1b4c2
landris006 <laczko.andris007@gmail.com>
2024-07-31 21:34:14 +0200

    fix client build

--- server/src/api/router.ts
+++ server/src/api/router.ts
@@ -1,6 +1,7 @@
 import { createRouter } from './trpc';
 import { topicRouter } from './routes/topic';
 import { instructorRouter } from './routes/instructor';
+import { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
 
 export const router = createRouter({
   topic: topicRouter,
@@ -8,3 +9,5 @@ export const router = createRouter({
 });
 
 export type Router = typeof router;
+export type RouterInput = inferRouterInputs<Router>;
+export type RouterOutput = inferRouterOutputs<Router>;
