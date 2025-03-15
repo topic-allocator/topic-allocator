031e38d681be2d0364719401fe921ba154bc9c73
landris006 <laczko.andris007@gmail.com>
2025-03-15 13:31:13 +0100

    solver flake + flake update [skip ci]

--- flake.nix
+++ flake.nix
@@ -1,15 +1,13 @@
 {
-  description = "A very basic flake";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
-    self,
     nixpkgs,
     utils,
+    ...
   }:
     utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
@@ -20,6 +18,9 @@
           playwright-driver.browsers # must match the version used in `e2e/package.json`
           openssl
           nodePackages.prisma
+
+          azure-cli
+          azure-functions-core-tools
         ];
 
         shellHook = ''
---
+++ solver/flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    utils,
+    ...
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      python = pkgs.python314;
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          python
+
+          mypy
+          ruff
+          black
+          pyright
+
+          azure-cli
+          azure-functions-core-tools
+        ];
+
+        shellHook = ''
+          VENV=.venv
+
+          if test ! -d $VENV; then
+            ${python}/bin/python -m venv $VENV
+          fi
+          source ./$VENV/bin/activate
+
+          export PYTHONPATH=`pwd`/$VENV/${python.sitePackages}/:$PYTHONPATH
+
+          pip install -r requirements.txt
+        '';
+
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
+          stdenv.cc.cc
+        ]);
+
+        # postShellHook = ''
+        #   ln -sf ${python.sitePackages}/* ./.venv/lib/python3.12/site-packages
+        # '';
+      };
+    });
+}
