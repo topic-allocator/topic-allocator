60db89ada541e41b05b6bea639a6733fdf1a3c5c
landris006 <laczko.andris007@gmail.com>
2025-03-15 12:30:46 +0100

    proxy fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:7071/',
+        target: 'http://127.0.0.1:7071/',
       },
     },
   },
